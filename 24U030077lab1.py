dee4df1cc0e240891d7da8da07de51ecfebfa507
devsharma-data-scientist <devbhraman5@gmail.com>
2025-07-29 22:41:17 +0530

    Add files via upload

---
+++ 24U030077lab1.py
@@ -0,0 +1,26 @@
+def Grade():
+    while True:
+        p = input("Enter Your Marks and to Exit write EXIT: ")
+
+        if p.lower() == "exit":
+            print("EXIT SUCCESSFUL")
+            break
+
+        try:
+            a = int(p)
+            if a >= 0 and a < 40:
+                print("F")
+            elif a >= 40 and a < 60:
+                print("D")
+            elif a >= 60 and a < 75:
+                print("C")
+            elif a >= 75 and a < 90:
+                print("B")
+            elif a >= 90 and a <= 100:
+                print("A")
+            else:
+                print("INVALID INPUT!")  
+        except:
+            print("INVALID INPUT!")
+
+Grade()
