5adcbf0e1c709c5e515c912742965be4370d32b7
devsharma-data-scientist <devbhraman5@gmail.com>
2025-07-29 22:57:44 +0530

    24U030077_lab1.py

--- lab1.py
+++ lab1.py
@@ -1,3 +1,28 @@
-#Write your code here in python 
-n=int(input("enter a nuber"))
-print(n)
+def Grade():      #function "Grade()"
+    while True:
+        p = input("Enter Your Marks and to Exit write EXIT: ")
+
+        if p.lower() == "exit":
+            print("EXIT SUCCESSFUL")
+            break
+
+        try:
+            a = int(p)    #input ko integer me convert karne ki koshish kro
+            if a >= 0 and a < 40:
+                print("F")   #Fail
+            elif a >= 40 and a < 60:
+                print("D")   #Below avg
+            elif a >= 60 and a < 75:
+                print("C")   #avg
+            elif a >= 75 and a < 90:
+                print("B")   #good
+            elif a >= 90 and a <= 100:
+                print("A")   #Excellent
+            else:
+                print("INVALID INPUT!")   # agr no. 0-100 ke bahar ho
+        except:
+            # agr input no. nahi hai,toh error!
+            print("INVALID INPUT!")
+
+Grade()
+#End of program
