dde8667b3fca0795b3f101713f85e54acb07f9ee
devsharma-data-scientist <devbhraman5@gmail.com>
2025-07-31 18:53:59 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,27 +1,22 @@
-while True:
-    p = input("Enter Your Marks and to Exit write EXIT: ")
-
-    if p.lower() == "exit":
-        print("EXIT SUCCESSFUL")
-        break
-
-    try:
-        a = int(p)    #input ko integer me convert karne ki koshish kro
-        if a >= 0 and a < 40:
-            print("F")   #Fail
-        elif a >= 40 and a < 60:
-            print("D")   #Below avg
-        elif a >= 60 and a < 75:
-            print("C")   #avg
-        elif a >= 75 and a < 90:
-            print("B")   #good
-        elif a >= 90 and a <= 100:
-            print("A")   #Excellent
-        else:
-            print("INVALID INPUT!")   # agr no. 0-100 ke bahar ho
-    except:
-        # agr input no. nahi hai,toh error!
-        print("INVALID INPUT!")
-
-Grade()
-#End of program
+p = input()
+if p.lower() == "exit":
+    print("EXIT SUCCESSFUL")
+    break
+    
+try:
+    a = int(p)    #input ko integer me convert karne ki koshish kro
+    if a >= 0 and a < 40:
+        print("F")   #Fail
+    elif a >= 40 and a < 60:
+        print("D")   #Below avg
+    elif a >= 60 and a < 75:
+        print("C")   #avg
+    elif a >= 75 and a < 90:
+        print("B")   #good
+    elif a >= 90 and a <= 100:
+        print("A")   #Excellent
+    else:
+        print("INVALID INPUT!")   # agr no. 0-100 ke bahar ho
+except:
+    # agr input no. nahi hai,toh error!
+     print("INVALID INPUT!")
