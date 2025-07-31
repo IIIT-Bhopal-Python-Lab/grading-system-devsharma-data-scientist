298372e57a90c3d5e1c01629c1a578a23879f714
devsharma-data-scientist <devbhraman5@gmail.com>
2025-07-31 19:25:08 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,27 +1,20 @@
-def Grade():
-    while True:
-        p = input("Enter Your Marks and to Exit write EXIT: ")
-
-        if p.lower() == "exit":
-            print("EXIT SUCCESSFUL")
-            break
-
-        if not p.isdigit():
-            print("INVALID INPUT!")
-            continue
-
+def Grade(p):
+    if isinstance(p, str) and p.lower() == "exit":
+        return "EXIT SUCCESSFUL"
+    
+    try:
         a = int(p)
         if a >= 0 and a < 40:
-            print("F")
+            return "F"
         elif a >= 40 and a < 60:
-            print("D")
+            return "D"
         elif a >= 60 and a < 75:
-            print("C")
+            return "C"
         elif a >= 75 and a < 90:
-            print("B")
+            return "B"
         elif a >= 90 and a <= 100:
-            print("A")
+            return "A"
         else:
-            print("INVALID INPUT!")
-
-Grade()
+            return "INVALID INPUT!"
+    except:
+        return "INVALID INPUT!"
