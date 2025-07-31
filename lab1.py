4ce5d076205b61027aaf213a76041d77d04a210b
devsharma-data-scientist <devbhraman5@gmail.com>
2025-07-31 19:22:25 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
-def Grade():      #function "Grade()"
+def Grade():
     while True:
         p = input("Enter Your Marks and to Exit write EXIT: ")
 
@@ -6,21 +6,22 @@ def Grade():      #function "Grade()"
             print("EXIT SUCCESSFUL")
             break
 
-        try:
-            a = int(p)    #input ko integer me convert krne ki koshish kro
-            if a >= 0 and a < 40:
-                print("F")   #Fail
-            elif a >= 40 and a < 60:
-                print("D")   #
-            elif a >= 60 and a < 75:
-                print("C")
-            elif a >= 75 and a < 90:
-                print("B")
-            elif a >= 90 and a <= 100:
-                print("A")
-            else:
-                print("INVALID INPUT!")  
-        except:
+        if not p.isdigit():
+            print("INVALID INPUT!")
+            continue
+
+        a = int(p)
+        if a >= 0 and a < 40:
+            print("F")
+        elif a >= 40 and a < 60:
+            print("D")
+        elif a >= 60 and a < 75:
+            print("C")
+        elif a >= 75 and a < 90:
+            print("B")
+        elif a >= 90 and a <= 100:
+            print("A")
+        else:
             print("INVALID INPUT!")
 
 Grade()
