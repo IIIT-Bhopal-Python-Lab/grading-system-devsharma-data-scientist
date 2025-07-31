b8668a7434cdfa72e106d19d53374c76f13148d0
devsharma-data-scientist <devbhraman5@gmail.com>
2025-07-31 19:00:25 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,12 +1,8 @@
-p = input()
-if p.lower() == "exit":
-    print("EXIT SUCCESSFUL")
-    break
-    
-try:
-    a = int(p)    #input ko integer me convert karne ki koshish kro
+a = input("Enter Your Marks(0-100):") #input ko integer me convert karne ki koshish kro
+if a.isdigit():
+    a = int(a)
     if a >= 0 and a < 40:
-        print("F")   #Fail
+    print("F")   #Fail
     elif a >= 40 and a < 60:
         print("D")   #Below avg
     elif a >= 60 and a < 75:
@@ -16,7 +12,6 @@ try:
     elif a >= 90 and a <= 100:
         print("A")   #Excellent
     else:
-        print("INVALID INPUT!")   # agr no. 0-100 ke bahar ho
-except:
-    # agr input no. nahi hai,toh error!
-     print("INVALID INPUT!")
+        print("INVALID INPUT!")  
+else:
+    print("INVALID INPUT")
