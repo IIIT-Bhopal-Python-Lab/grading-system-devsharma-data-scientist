a570d259a8073ce31127f8e6507002085c6de3a6
devsharma-data-scientist <devbhraman5@gmail.com>
2025-07-31 19:05:44 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,17 +1,26 @@
-a = input("Enter Your Marks(0-100):") #input ko integer me convert karne ki koshish kro
-if a.isdigit():
-    a = int(a)
-    if a >= 0 and a < 40:
-    print("F")   #Fail
-    elif a >= 40 and a < 60:
-        print("D")   #Below avg
-    elif a >= 60 and a < 75:
-        print("C")   #avg
-    elif a >= 75 and a < 90:
-        print("B")   #good
-    elif a >= 90 and a <= 100:
-        print("A")   #Excellent
-    else:
-        print("INVALID INPUT!")  
-else:
-    print("INVALID INPUT")
+def Grade():      #function "Grade()"
+    while True:
+        p = input("Enter Your Marks and to Exit write EXIT: ")
+
+        if p.lower() == "exit":
+            print("EXIT SUCCESSFUL")
+            break
+
+        try:
+            a = int(p)    #input ko integer me convert krne ki koshish kro
+            if a >= 0 and a < 40:
+                print("F")   #Fail
+            elif a >= 40 and a < 60:
+                print("D")   #
+            elif a >= 60 and a < 75:
+                print("C")
+            elif a >= 75 and a < 90:
+                print("B")
+            elif a >= 90 and a <= 100:
+                print("A")
+            else:
+                print("INVALID INPUT!")  
+        except:
+            print("INVALID INPUT!")
+
+Grade()
